fa22003ace1ac669e6575e8925c0c5da246a1cb8
Acheley Saint Vilien <acheleysaintvilien@Acheleys-MacBook-Air.local>
2022-03-29 15:37:24 -0400

    first commit

---
+++ posts/views.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.shortcuts import  redirect, render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post
+from .forms import PostForm
+from cloudinary.forms import cl_init_js_callbacks
+# Create your views here.
+
+
+def index(request):
+    form = PostForm(request.POST, request.FILES)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect(form.errors.as_json())
+
+    posts = Post.objects.all()[:20]
+    return render(request, 'posts.html', {'posts': posts, 'form': form})
+
+
+def delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
+
+def edit(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect(form.errors.as_json())
+    form = PostForm
+    return render(request, 'edit.html', {'post': post, 'form': form})
+
+def likes(request, id):
+    Likedtweet = Post.objects.get(id=id)
+    new_value = Likedtweet.like_count + 1    
+    Likedtweet.like_count = new_value
+    Likedtweet.save()
+    return HttpResponseRedirect('/')
